6fc0175e789579036f085932b722f393ba0cc52a
HubertG97 <0892400@hr.nl>
2016-09-22 14:10:57 +0200

    routes testing

--- project/routes/web.php
+++ project/routes/web.php
@@ -12,5 +12,34 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    echo 'Hello World';
+});
+
+Route::get('hello', function(){
+    echo 'Hello again ';
+});
+
+//create item
+Route::post('test', function(){
+    echo 'we just created an item';
+});
+
+//read item
+Route::get('test', function(){
+    echo '<form action="test" method="POST">';
+    echo '<input type="submit">';
+    echo '<input type="hidden" value="' . csrf_token() . '" name="_token">';
+    echo '<input type="hidden" name="_method" value="PUT">';
+    echo '</form>';
+});
+
+//update item
+Route::put('test', function(){
+    echo 'we have just updated an item';
+});
+
+//delete item
+Route::delete('test', function(){
+    echo 'we have just deleted an item';
+
 });
