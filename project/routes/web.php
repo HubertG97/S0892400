fc71ccc4956436c102a2b180d5459130bd085fc8
HubertG97 <0892400@hr.nl>
2016-09-22 19:24:40 +0200

    Laravel: Database
    added database and model for the project

--- project/routes/web.php
+++ project/routes/web.php
@@ -15,31 +15,36 @@ Route::get('/', function () {
     echo 'Hello World';
 });
 
-Route::get('hello', function(){
-    echo 'Hello again ';
+Route::get('festival/{id}', function($id){
+    $festival = App\Festival::find($id);
+    echo $festival->name;
 });
 
-//create item
-Route::post('test', function(){
-    echo 'we just created an item';
-});
-
-//read item
-Route::get('test', function(){
-    echo '<form action="test" method="POST">';
-    echo '<input type="submit">';
-    echo '<input type="hidden" value="' . csrf_token() . '" name="_token">';
-    echo '<input type="hidden" name="_method" value="PUT">';
-    echo '</form>';
-});
-
-//update item
-Route::put('test', function(){
-    echo 'we have just updated an item';
-});
-
-//delete item
-Route::delete('test', function(){
-    echo 'we have just deleted an item';
-
-});
+//Route::get('hello', function(){
+//    echo 'Hello again ';
+//});
+//
+////create item
+//Route::post('test', function(){
+//    echo 'we just created an item';
+//});
+//
+////read item
+//Route::get('test', function(){
+//    echo '<form action="test" method="POST">';
+//    echo '<input type="submit">';
+//    echo '<input type="hidden" value="' . csrf_token() . '" name="_token">';
+//    echo '<input type="hidden" name="_method" value="PUT">';
+//    echo '</form>';
+//});
+//
+////update item
+//Route::put('test', function(){
+//    echo 'we have just updated an item';
+//});
+//
+////delete item
+//Route::delete('test', function(){
+//    echo 'we have just deleted an item';
+//
+//});
