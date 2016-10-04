6738d1b78b21184097a54926b1b19df916252e18
HubertG97 <0892400@hr.nl>
2016-10-04 13:46:05 +0200

    commit

--- project/routes/web.php
+++ project/routes/web.php
@@ -12,7 +12,7 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
 Route::get('/redirect', 'SocialAuthController@redirect');
 Route::get('/callback', 'SocialAuthController@callback');
